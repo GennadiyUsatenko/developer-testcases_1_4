95ab1890299ca0fadf945b771be3c4a2b180f85d
GenaPC <usatenko.gena@gmail.com>
2018-09-18 10:52:53 +0300

    Initial commit

---
+++ src/main/java/com/paazl/cases/test1/Main.java
@@ -0,0 +1,14 @@
+package com.paazl.cases.test1;
+
+public class Main {
+	/*
+	 * TODO
+	 * Implement the main method:
+	 * * Show the user the questions from Constants.QUESTIONS on a console and collect the answers.
+	 * * If the user scores 0-3 points, print "You are a junior Java developer".
+	 * * If the user scores 4-7 points, print "You are a medior Java developer".
+	 * * If the user scores 8-10 points, print "You are a senior Java developer".
+	 */
+	public static void main(String[] args) {
+	}
+}
---
+++ src/main/java/com/paazl/cases/test2/Main.java
@@ -0,0 +1,15 @@
+package com.paazl.cases.test2;
+
+import java.io.IOException;
+
+public class Main {
+    /*
+     * TODO
+     * Implement a main method that is functionally identical to Test #1. In
+     * this case, use a DeveloperFactory that is able to produce 3 types of
+     * developers that implement a "print" method. The Factory should have a
+     * create method that takes the user's score as an argument.
+     */
+    public static void main(String[] args) throws IOException {
+    }
+}
